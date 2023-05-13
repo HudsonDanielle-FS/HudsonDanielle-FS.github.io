9c1ef3b9713285ba0cbea848de9fbdde9fed3343
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-05-13 13:40:07 -0500

    removed unneeded components/ changed styling

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        {/* resume pdf goes in the href below without '' */}
-        {/* will have to import cv from assets folder */} 
-        <a href={''} download className='btn'>Download CV</a>
+        <a href="#portfolio" className='btn'>My Portfolio</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
