15bfb067a48f8c778ed534a798479f24a3e28e2f
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-05-16 11:18:00 -0500

    portfolio section complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
 import './portfolio.css'
+import Placeholder from '../../assets/placeholder.png'
+
+const data = [
+  {
+    id: 1,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  },
+  {
+    id: 2,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  },
+  {
+    id: 3,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  },
+  {
+    id: 4,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  },
+  {
+    id: 5,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  },
+  {
+    id: 6,
+    image: Placeholder,
+    title: 'This is a portfolio title',
+    github: 'https://github.com',
+    demo: 'http://dribbble.com',
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <div className='portfolio__item-cta'>
+            <a href={github} className='btn' target="_blank" rel="noreferrer">Github</a>
+            <a href={demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+            </div>
+          </div>
+        </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
