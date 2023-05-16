5670cf469493f5b096c34c7a2a928e132e73ba3c
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-05-16 13:14:04 -0500

    contact component complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './contact.css'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lmerza8', 'template_sszvb4t', form.current, 'aA_beqR7qKcB59ZeI')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email'name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div id='home' className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Kaden Hudson</h1>
-        <h5 className="text-light">Frontend Developer</h5>
+        <h5 className="text-light">Web Developer</h5>
         <CTA />
 
         <div className="me">
